1b260470cdcdeffd6745da2f58472d0a5250f9cc
Solovev Alexander <solovyov_alex@mail.ru>
2024-12-22 19:26:02 +0500

    web-ip-hw

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
-git clone https://github.com/hachubra/shvirtd-example-python.git /opt/pyth
-docker compose -f /opt/pyth/compose.yaml p -d
+#!/bin/bash
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# newgrp docker
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+sudo rm /opt/pyth/ --force --recursive && sudo git clone https://github.com/hachubra/shvirtd-example-python.git /opt/pyth && docker compose -f /opt/pyth/compose.yml up -d
